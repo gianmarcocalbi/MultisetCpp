d06cc27abd871956128213e1474db0ae7b248968
grimidev <calbigianmarco@gmail.com>
2016-03-31 18:02:26 +0200

    Const_iterator added.

--- MultisetCpp/main.cpp
+++ MultisetCpp/main.cpp
@@ -9,7 +9,37 @@ int main() {
 	m1.remove(2);
 	m1.add(3);
 	m1.remove(199);
-	m1.print();
+	//m1.print();
+
+	multiset<int> m2 = multiset<int>();
+	m2.add(2);
+	m2.add(2);
+	m2.add(2);
+	m2.add(199);
+	m2.remove(2);
+	m2.add(3);
+	m2.remove(199);
+	//m2.print();
+
+	multiset<int> m3 = multiset<int>();
+	m3.add(3);
+	m3.add(3);
+	m3.add(3);
+	m3.add(4);
+	m3.add(5);
+	m3.add(5);
+	m3.add(6);
+	//m2.print();
+
+	//cout << (m1 == m2) << endl;
+	
+	multiset<int>::const_iterator it1 = m3.begin();
+
+	for (; it1 != m3.end(); ++it1) {
+		std::cout << *it1 << std::endl;
+	}
+	
+
 	system("pause");
 	return 0;
 }
--- MultisetCpp/multiset.h
+++ MultisetCpp/multiset.h
@@ -122,29 +122,73 @@ public:
 	}
 
 	/**
-	* Copy Constructor.
-	*/
+	 * Copy Constructor.
+	 */
+	multiset(const multiset &other) : head(0), objects_amount(0), nodes_amount(0) {
+		node *tmp = other.head;
+		try {
+			while (temp != 0) {
+				add(temp->object, temp->amount);
+				temp = temp->next;
+			}
+		} catch (...) {
+			clear();
+			throw;
+		}
+	}
 
 	/**
-	* Operatore Assegnamento.
-	*/
+	 * Operatore Assegnamento.
+	 */
+	multiset &operator=(const multiset &other) {
+		if (this != other) {
+			multiset tmp(other);
+			std::swap(this->head, temp.head);
+			std::swap(this->objects_amount, objects_amount);
+			std::swap(this->nodes_amount, nodes_amount);
+		}
+		return *this;
+	}
 
 	/**
-	* Operatore Assegnamento Templato.
-	*/
+	 * //(!)Operatore Assegnamento Templato.
+	 */
+	/*template<typename Q>
+	multiset &operator=(const multiset<Q> &other) {
+		multiset<T> ms;
+		try {
+			node *tmp = other.get_head();
+			while (tmp != 0) {
+				ms.add(static_cast<T>(tmp->object), tmp->amount);
+				tmp->next;
+			}
+		} catch (...) {
+			cout << "ERRORE: static_cast fallito." << endl;
+			throw;
+		}
+		return *this;
+	}*/
 
 	/**
 	 * Add.
 	 * Operazione di aggiunta elemento.
 	 */
 	void add(const T &obj) {
+		add(obj, 1);
+	}
+
+	/**
+	* Add overload.
+	* Operazione di aggiunta elemento.
+	*/
+	void add(const T &obj, size_type n) {
 		node *t = head;
 		bool found = false;
 		while (t != 0) {
 			if (t->object == obj) {
-				//se l'oggetto è già presente allora aumenta
+				//se l'oggetto è già presente allora aumentane la quantità di n
 				found = true;
-				t->amount++;
+				t->amount += n;
 				t = 0;
 			} else {
 				t = t->next;
@@ -154,14 +198,16 @@ public:
 			//altrimenti aggiungi nodo
 			if (head == 0) {
 				head = new node(obj);
+				head->amount = n;
 			} else {
 				node *tmp = head;
 				head = new node(obj, tmp);
+				head->amount = n;
 				tmp->prev = head;
 			}
 			nodes_amount++;
 		}
-		objects_amount++;
+		objects_amount += n;
 	}
 
 	/**
@@ -197,23 +243,88 @@ public:
 	}
 
 	/**
-	 * Numero elementi del multiset (contando i duplicati).
-	 */
+	* Numero elementi del multiset (contando i duplicati).
+	*/
 	size_type get_objects_amount() {
 		return objects_amount;
 	}
 
 	/**
-	 * Numero nodi nel multiset (numero di oggetti senza contare duplicati).
-	 */
+	* Numero elementi del multiset (contando i duplicati).
+	*/
+	size_type get_objects_amount() const {
+		return objects_amount;
+	}
+
+	/**
+	* Numero nodi nel multiset (numero di oggetti senza contare duplicati).
+	*/
 	size_type get_nodes_amount() {
 		return nodes_amount;
 	}
 
 	/**
-	 * Conta numero di occorrenze di un oggetto nel multiset.
-	 */
+	* Numero nodi nel multiset (numero di oggetti senza contare duplicati).
+	*/
+	size_type get_nodes_amount() const {
+		return nodes_amount;
+	}
+
+	/**
+	* Conta numero di occorrenze di un oggetto nel multiset.
+	*/
 	size_type object_count(T &obj) {
+		node *tmp = head;
+		size_type count = 0;
+		while (tmp != 0) {
+			if (obj == tmp->object) {
+				count = tmp->amount;
+				tmp = 0;
+			} else {
+				tmp = tmp->next;
+			}
+		}
+		return count;
+	}
+
+	/**
+	* Conta numero di occorrenze di un oggetto nel multiset.
+	*/
+	size_type object_count(T &obj) const {
+		node *tmp = head;
+		size_type count = 0;
+		while (tmp != 0) {
+			if (obj == tmp->object) {
+				count = tmp->amount;
+				tmp = 0;
+			} else {
+				tmp = tmp->next;
+			}
+		}
+		return count;
+	}
+
+	/**
+	* Conta numero di occorrenze di un oggetto nel multiset.
+	*/
+	size_type object_count(const T &obj) {
+		node *tmp = head;
+		size_type count = 0;
+		while (tmp != 0) {
+			if (obj == tmp->object) {
+				count = tmp->amount;
+				tmp = 0;
+			} else {
+				tmp = tmp->next;
+			}
+		}
+		return count;
+	}
+
+	/**
+	* Conta numero di occorrenze di un oggetto nel multiset.
+	*/
+	size_type object_count(const T &obj) const {
 		node *tmp = head;
 		size_type count = 0;
 		while (tmp != 0) {
@@ -246,6 +357,10 @@ public:
 		return object_count(obj) > 0;
 	}
 
+	/**
+	 * Operatore ==.
+	 *
+	 */
 	bool &operator== (const multiset &other) {
 		bool equals = true;
 		if (objects_amount == other.get_objects_amount() && nodes_amount == other.get_nodes_amount()) {
@@ -264,7 +379,126 @@ public:
 		return equals;
 	}
 
-	//Forward Iterators
+	//Forward Iterators Const
+	/**
+	 * Forward Iterator di sola lettura
+	 */
+	class const_iterator;
+
+	class const_iterator {
+	private:
+		friend class multiset<T>;
+		const node *curr;
+		size_type n;
+
+		const_iterator(const node *ptr) {
+			curr = ptr;
+			n = 0;
+		}
+
+	public:
+
+		/**
+		 * Costruttore di default.
+		 */
+		const_iterator() : curr(0), n(0) { }
+
+		/**
+		* Copy Constructor.
+		*/
+		const_iterator(const const_iterator &other) : curr(other.curr), n(other.n) { }
+
+		/**
+		* Operatore Assegnamento.
+		*/
+		const_iterator& operator= (const const_iterator &other){
+			curr = other.curr;
+			n = other.n;
+			return *this;
+		}
+
+		/**
+		* Distruttore.
+		*/
+		~const_iterator() { }
+
+		/**
+		* Dereferenziamento per reference.
+		*/
+		const T &operator*() const {
+			return curr->object;
+		}
+
+		/**
+		* Dereferenziamento per puntatore.
+		*/
+		const T * operator->() const {
+			return &(curr->object);
+		}
+
+		/**
+		 * Post-incremento
+		 * @return l'iteratore alla pair precedente
+		 */
+		const_iterator operator++(int) {
+			const_iterator tmp(*this);
+			if (n == curr->amount - 1) {
+				curr = curr->next;
+				n = 0;
+			} else {
+				n++;
+			}
+			return tmp;
+		}
+
+		/**
+		 * Pre-incremento
+		 * @return l'iteratore alla pair successiva
+		 */
+		const_iterator& operator++() {
+			if (n == curr->amount-1) {
+				curr = curr->next;
+				n = 0;
+			} else {
+				n++;
+			}
+			return *this;
+		}
+
+		/**
+		Confronto (uguaglianza)
+		@other iteratore da confrontare
+		@return true se i due iteratori puntano alla stessa posizione
+		*/
+		bool operator==(const const_iterator &other) const {
+			return curr == other.curr && n == n.other;
+		}
+
+		/**
+		Confronto (diversita')
+		@other iteratore da confrontare
+		@return false se i due iteratori puntano alla stessa posizione
+		*/
+		bool operator!=(const const_iterator &other) const {
+			return curr != other.curr || n != other.n;
+		}
+	};
+
+	/**
+	* Iteratore di inizio sequenza
+	* @return l'iteratore di inizio sequenza
+	*/
+	const_iterator begin() const {
+		return const_iterator(head);
+	}
+
+	/**
+	* Iteratore di fine sequenza
+	* @return l'iteratore di fine sequenza
+	*/
+	const_iterator end() const {
+		return const_iterator(0);
+	}
 
 	//DEBUG
 	/**
